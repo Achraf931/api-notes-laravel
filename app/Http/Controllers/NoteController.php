ba2ae75ea289f785c7b5dca0446fc1c126643ba2
Achraf <hamrouni.pro@outlook.fr>
2020-04-20 16:54:03 +0200

    first content

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NoteRequest;
+use App\Note;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use PHPUnit\Exception;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        $result = Note::orderBy('created_at', 'desc')->get();
+        if (empty($result)) {
+            return response()->json(['error' => 'Nothing to get']);
+        }
+
+        try {
+            return response()->json(['notes' => $result, 'error' => null]);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Nothing to get'], 404);
+        }
+    }
+
+    public function store(NoteRequest $request)
+    {
+        try {
+            return response()->json(['note' => Note::create($request->all()), 'error' => null]);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cet identifiant est inconnu'], 404);
+        }
+    }
+
+    public function update(NoteRequest $request, $id)
+    {
+        try {
+            Note::findOrFail($id)->update($request->all());
+            return response()->json(['note' => Note::where('id', $id)->get()->first(), 'error' => null]);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cet identifiant est inconnu'], 404);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            return response()->json(['note' => Note::findOrFail($id), 'error' => null]);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Cet identifiant est inconnu'], 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            Note::findOrFail('id', $id)->delete();
+            return response()->json(['error' => null]);
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'Cet identifiant est inconnu'], 404);
+        }
+    }
+}
